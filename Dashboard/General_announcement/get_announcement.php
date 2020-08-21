afd16a4ddfa294cbaaf1da270627ae756ad35a98
anassKHALIS <64774939+anassKHALIS@users.noreply.github.com>
2020-08-21 11:51:12 +0200

    Add files via upload

---
+++ Dashboard/General_announcement/get_announcement.php
@@ -0,0 +1,33 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "solid";
+
+$connection_String = mysqli_connect($host,$user,$pass,$database);
+ $get_all_announcement_command = "SELECT * FROM general_announcement ORDER BY id DESC";
+ $execute_get_announcement_query = mysqli_query($connection_String,$get_all_announcement_command);
+
+
+ while( $get_rows = mysqli_fetch_assoc($execute_get_announcement_query)){
+
+ echo "<div class='col-lg-4 col-md-4 col-sm-6'>";
+ echo "<div class='panel panel-decorated'>
+           <div class = 'panel-heading'>
+         <h4 class = 'panel-title ptitle'>".$get_rows["message_title"]."</h4>
+        </div>";
+
+
+       
+$lien = $get_rows["message_body"];
+
+  echo  "<div class = 'panel-body pbody'> <a href='../Jeux/$lien'>
+         <span>".$get_rows["message_body"]."</span></a>
+         <br><br>
+          </div>
+       </div>
+       </div>";
+     }
+
+ ?>
